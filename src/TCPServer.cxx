5727bc5d8572283216c6c229f1a0d73e45af848e
Christopher Hunt <christopher.hunt@loadlock.co.uk>
2021-01-26 17:36:57 +0000

    Added some bufferevent and listener event tests

--- src/TCPServer.cxx
+++ src/TCPServer.cxx
@@ -7,37 +7,19 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <event2/event.h>
 #include <event2/buffer.h>
+#include <event2/util.h>
 #include <event2/bufferevent.h>
+#include <event2/listener.h>
 #include <cassert>
 #include <exception>
 #include <iostream>
 
 
-const char* success_string = "Message received!";
-
-
-struct fd_state
-{
-  char buffer[MAX_LINE];
-  size_t buffer_used;
-
-  size_t n_written;
-  size_t write_upto;
-
-  event* read_event;
-  event* write_event;
-};
-
-
-void do_read( evutil_socket_t, short, void* );
-void do_write( evutil_socket_t, short, void* );
-void do_accept( evutil_socket_t, short, void* );
-
-
-fd_state* allocate_fd_state( event_base*, evutil_socket_t );
-void free_fd_state( fd_state* );
+void read_cb( bufferevent* bev, void* ctx );
+void event_cb( bufferevent* bev, short events, void* ctx );
+void accept_cb( evconnlistener* listener, evutil_socket_t fd, sockaddr* addr, int addrlen, void* ctx );
+void error_cb( evconnlistener* bev, void* ctx );
 
 
 
@@ -45,53 +27,43 @@ int main( int, char** )
 {
   std::cout << "Good morning Q. Waiting for messages." << std::endl;
 
-
   // Listening port number
   int port_number = MONEYPENNY_PORT_NUMBER;
 
-  // Listener FD
-  evutil_socket_t listener;
+  // Listener event
+  evconnlistener* listener;
 
-  // Server address
-  sockaddr_in server_address;
+  // Socket address
+  sockaddr_in address;
 
-  // Event pointers
+  // Event base pointer
   event_base* base = event_base_new();
-  event* listener_event;
 
-  // Check it succeeded.
-  assert( base );
-
-  // Set the server address family
-  server_address.sin_family = AF_INET;
-  // Set the port number, checking that it is in network byte order.
-  server_address.sin_port = htons( port_number );
-  // Set the address. Host machine uses the symnbolic value.
-  server_address.sin_addr.s_addr = INADDR_ANY;
-
-
-  // Create the socket
-  listener = socket( AF_INET, SOCK_STREAM, 0 );
-  // Make it non-blocking
-  evutil_make_socket_nonblocking( listener );
-
-
-  // Bind the socket to the address and check for success
-  if ( bind( listener, (sockaddr*)&server_address, sizeof( server_address ) ) < 0 )
+  if ( base == nullptr )
   {
-    throw std::runtime_error( "Failed to bind socket to address" );
+    std::cerr << "Could not create event base" << std::endl;
+    return 1;
   }
 
-  listen( listener, 16 );
+  // Set the server address family
+  address.sin_family = AF_INET;
+  // Set the port number, checking that it is in network byte order.
+  address.sin_port = htons( port_number );
+  // Set the address. Host machine uses the symnbolic value.
+  address.sin_addr.s_addr = INADDR_ANY;
 
-  listener_event = event_new( base, listener, EV_READ|EV_PERSIST, do_accept, (void*)base );
 
-  event_add( listener_event, nullptr );
+  listener = evconnlistener_new_bind( base, accept_cb, nullptr, LEV_OPT_CLOSE_ON_FREE|LEV_OPT_REUSEABLE, -1, (sockaddr*)&address, sizeof(address) );
+
+  if ( listener == nullptr )
+  {
+    std::cerr << "Could not create listener" << std::endl;
+    return 1;
+  }
+
+  evconnlistener_set_error_cb( listener, error_cb );
 
   event_base_dispatch( base );
-
-  event_free( listener_event );
-  event_base_free( base );
 
 
   libevent_global_shutdown();
@@ -101,217 +73,43 @@ int main( int, char** )
 }
 
 
-
-void do_read( evutil_socket_t fd, short /*events*/, void* arg )
+void read_cb( bufferevent* bev, void* ctx )
 {
-  fd_state* state = (fd_state*)arg;
-  char buf[1024];
-  int i;
-  ssize_t result;
+  evbuffer* input = bufferevent_get_input( bev );
+  evbuffer* output = bufferevent_get_output( bev );
 
-  std::cout << "Read Event" << std::endl;
-
-  while ( true )
-  {
-    assert( state->write_event );
-    result = read( fd, buf, sizeof( buf )-1 );
-    if ( result <= 0 )
-    {
-      break;
-    }
-
-    for ( i = 0; i < result; ++i )
-    {
-      if ( state->buffer_used < sizeof( state->buffer ) )
-      {
-        state->buffer[state->buffer_used++] = buf[i];
-      }
-      if ( buf[i] == '\n' || buf[i] == '\0' )
-      {
-        assert( state->write_event );
-        event_add( state->write_event, nullptr );
-        state->write_upto = state->buffer_used;
-      }
-    }
-
-    std::cout << "Received message: " << buf << std::endl;
-  }
-
-  event_del( state->read_event );
-
-  if ( result == 0 )
-  {
-    free_fd_state( state );
-  }
-  else if ( result < 0 )
-  {
-    if ( errno == EAGAIN ) 
-      return;
-    else
-    {
-      // Oh fuck.
-      free_fd_state( state );
-    }
-  }
+  evbuffer_add_buffer( output, input );
 }
 
 
-void do_write( evutil_socket_t fd, short /*events*/, void* arg )
+void event_cb( bufferevent* bev, short events, void* ctx )
 {
-  fd_state* state = (fd_state*)arg;
-  ssize_t result;
+  if ( events & BEV_EVENT_ERROR )
+    std::cerr << "Error from buffer event: " << EVUTIL_SOCKET_ERROR() << std::endl;
 
-  std::cout << "Write Event" << std::endl;
-
-  while ( state->n_written < state->write_upto )
-  {
-    result = write( fd, state->buffer + state->n_written, state->write_upto - state->n_written );
-
-    if ( result < 0 )
-    {
-      if ( errno == EAGAIN ) 
-      {
-        return;
-      }
-      else
-      {
-        free_fd_state( state );
-        return;
-      }
-    }
-    assert( result != 0 );
-    state->n_written += result;
-  }
-
-  if ( state->n_written == state->buffer_used )
-  {
-    state->n_written = 1;
-    state->write_upto = 1;
-    state->buffer_used = 1;
-  }
-
-  event_del( state->write_event );
-  free_fd_state( state );
-
+  if ( events & BEV_EVENT_EOF | BEV_EVENT_ERROR )
+    bufferevent_free( bev );
 }
 
 
-void do_accept( evutil_socket_t listener, short /*events*/, void* arg )
+void accept_cb( evconnlistener* listener, evutil_socket_t fd, sockaddr* addr, int addrlen, void* ctx )
 {
-  event_base* base = (event_base*)arg;
-  sockaddr_storage ss;
+  event_base* base = evconnlistener_get_base( listener );
+  bufferevent* bev = bufferevent_socket_new( base, fd, BEV_OPT_CLOSE_ON_FREE );
 
-  std::cout << "Accept Event" << std::endl;
-
-  socklen_t slen = sizeof( ss );
-
-  int fd = accept( listener, (sockaddr*)&ss, &slen );
-
-  if ( fd < 0 )
-  {
-    // Bugger!
-    return;
-  }
-  else if ( fd > FD_SETSIZE )
-  {
-    close( fd );
-  }
-  else
-  {
-    evutil_make_socket_nonblocking( fd );
-
-    fd_state* state = allocate_fd_state( base, fd );
-    assert( state );
-
-    event_add( state->read_event, nullptr );
-  }
+  bufferevent_setcb( bev, read_cb, nullptr, event_cb, nullptr );
+  bufferevent_enable( bev, EV_READ|EV_WRITE );
 }
 
 
-fd_state* allocate_fd_state( event_base* base, evutil_socket_t fd )
+void error_cb( evconnlistener* bev, void* ctx )
 {
-  fd_state* state = new fd_state();
+  event_base* base = evconnlistener_get_base( bev );
 
-  state->read_event = event_new( base, fd, EV_READ|EV_PERSIST, do_read, state );
-  state->write_event = event_new( base, fd, EV_WRITE|EV_PERSIST, do_write, state );
+  int err = EVUTIL_SOCKET_ERROR();
 
-  if ( state->read_event == nullptr || state->write_event == nullptr )
-  {
-    throw std::runtime_error( "Could not create read/write events." );
-  }
+  std::cerr << "Listener error occured : " << err;
 
-  state->buffer_used = state->n_written = state->write_upto = 0;
-
-  return state;
+  event_base_loopexit( base, nullptr );
 }
-
-
-void free_fd_state( fd_state* state )
-{
-  event_free( state->read_event );
-  event_free( state->write_event );
-  delete state;
-}
-
-
-/*
-  try
-  {
-    // Create a socket with an internet address
-    socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
-
-    // Check it was successful
-    if ( socket_fd < 0 )
-    {
-      throw std::runtime_error( "Failed to open socket." );
-    }
-
-    // Bind the socket to the address and check for success
-    if ( bind( socket_fd, (sockaddr*)&server_address, sizeof( server_address ) ) < 0 )
-    {
-      throw std::runtime_error( "Failed to bind socket to address" );
-    }
-
-
-    // Listen to the socket and wait for message. Queue connections 5 deep. This function cannot fail.
-    listen( socket_fd, 5 );
-
-    // Wait for a connection to the client
-    new_socket_fd = accept( socket_fd, (sockaddr*)&client_address, &client_address_size );
-
-    // Make sure we succeeded
-    if ( new_socket_fd < 0 )
-    {
-      throw std::runtime_error( "Could not connect to client." );
-    }
-
-    // Block until the client has written something. Then read it to the buffer
-    result = read( new_socket_fd, buffer, 255 );
-
-    if ( result < 0 )
-    {
-      throw std::runtime_error( "An error occured while reading from the socket" );
-    }
-
-
-    std::cout << "You have a message Mr. Bond:\n\n" << buffer << std::endl;
-
-
-    result = write( new_socket_fd, success_string, sizeof( success_string ) );
-
-    if ( result < 0 )
-    {
-      throw std::runtime_error( "An error occured while writing to the socket" );
-    }
-
-    close( new_socket_fd );
-    close( socket_fd );
-  }
-  catch( std::exception& ex )
-  {
-    std::cerr << "An error occured: " << ex.what() << std::endl;
-    return 1;
-  }
-
-*/
 
